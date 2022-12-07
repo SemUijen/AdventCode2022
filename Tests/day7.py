b57fe1246e29a76cb2fa88fb8c6a6e1a1b729d9d
SemUijen <grootveer.sem@outlook.com>
2022-12-07 22:43:38 +0100

    day7 update

--- Tests/day7.py
+++ Tests/day7.py
@@ -1,10 +1,50 @@
 lines = open('../data/day7_test.txt', 'r')
-#from functions.Tree import Tree, Node
-#Dir_Tree = Tree(Node(0, 0, None, "Start", 'Start', 0))
+from functions.Tree import Tree, Node
+
+
+def Create_tree():
+    dir_tree_dict = {}
+    dir_tree = Tree(Node(0, 0, None, "/", 'dir'))
+
+    current_nodeID = 0
+    for line in lines:
+        if 'cd' == line.split()[1]:
+            if '/' == line.split()[2]:
+                current_nodeID = dir_tree.getRoot().ID
+                current_node = dir_tree.find(current_nodeID)
+
+            if '..' == line.split()[2]:
+                size = current_node.Size
+                current_nodeID = current_node.Parent.ID
+                current_node = dir_tree.find(current_nodeID)
+                current_node.Size += size
+
+            elif not '/' == line.split()[2]:
+                current_nodeID = dir_tree_dict[line.split()[2]]
+                current_node = dir_tree.find(current_nodeID)
+
+        else:
+            if 'dir' in line:
+                current_node_id = current_node.add_Node(line.split()[1], 'dir')
+                dir_tree_dict[line.split()[1]] = current_node_id
+
+            elif '$ ls' != line.replace('\n', ''):
+
+                current_nodeID = current_node.add_Node(line.split()[1], 'file')
+                dir_tree_dict[line.split()[1]] = current_nodeID
+
+                dir_tree.find(current_nodeID).Size += int(line.split()[0])
+                current_node.Size += int(line.split()[0])
+
+    #print(dir_tree_dict)
+    #dir_tree.printTree()
+    return dir_tree
+
 
 def day7_part1():
+    dir_tree = Create_tree()
+    score = dir_tree.find_largeDir()
 
-    for line in lines:
-
+    return score
 
 day7_part1()
--- functions/Tree.py
+++ functions/Tree.py
@@ -1,7 +1,6 @@
-
 # Classes
 class Node:
-    def __init__(self, ID, level, Parent, Name, Type, Size):
+    def __init__(self, ID, level, Parent, Name, Type):
         self.children = []
         self.ID = ID
         self.Level = level
@@ -9,22 +8,18 @@ class Node:
 
         self.Name = Name
         self.Type = Type
-        self.Size = Size
+        self.Size = 0
 
     def Node_tostring(self):
-        String = ("Node ID: " + str(self.ID),
-                  "Node Level: " + str(self.Level),
-                  "Node Name: " + str(self.Name),
-                  "Node Type: " + str(self.Type),
-                  "Node Size: " + str(self.Size))
+        String = ('- ' + str(self.Name) + ' ' + str(self.Type) + ' ' + 'size: ' + str(self.Size) + ' ' + str(self.ID))
         String = str(String)
         return String
 
-    def add_Node(self,Name, Type, Size):
+    def add_Node(self, Name, Type):
         level = self.Level + 1
         ID = self.ID + (len(self.children) + 1) / 100 ** self.Level
         self.children.append(
-            Node(ID, level, self, Name, Type, Size))
+            Node(ID, level, self, Name, Type))
 
         return ID
 
@@ -43,22 +38,23 @@ class Tree:
             return "empty tree"
 
     def _find(self, val, node):
-
-        if (val == node.ID):
+        if val == node.ID:
             return node
 
         # if node has no children then ID is not in the tree
         if len(node.children) == 0:
             return print("error", val, "not found")
+
         # if node only has one child below find method doesnt work
         if len(node.children) == 1:
             return self._find(val, node.children[0])
 
         # loop through children of a node. val must be between IDs of two children(this is the way the tree is setup)
         for i in range(len(node.children) - 1):
-            if val >= node.children[i].ID and val < node.children[i + 1].ID:
+            if node.children[i].ID <= val < node.children[i + 1].ID:
                 return self._find(val, node.children[i])
-        return node.children[len(node.children) - 1]
+
+        return self._find(val, node.children[len(node.children) - 1])
 
     def deleteTree(self):
         # garbage collector will do this for us.
@@ -77,3 +73,26 @@ class Tree:
             for child in Node.children:
                 print(Spaces, child.Node_tostring())
                 self._printTree(child)
+
+    def find_largeDir(self):
+        score = 0
+        if self.root is not None:
+            if self.root.Size <= 100000:
+                score += self.root.Size
+
+            score = self._find_largeDir(self.root, score)
+
+        print(score)
+        return score
+
+    def _find_largeDir(self, node, score):
+
+        if node.children != []:
+            for child in node.children:
+
+                if child.Type == 'dir' and child.Size <= 100000:
+                    score += child.Size
+                score = self._find_largeDir(child, score)
+
+
+        return score
