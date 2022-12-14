e40434d7fd62501a9a0c641fe6b8f126c4912d9f
SemUijen <grootveer.sem@outlook.com>
2022-12-15 00:03:06 +0100

    update day9

--- Tests/day9.py
+++ Tests/day9.py
@@ -1,7 +1,9 @@
 import numpy as np
 from functions.day9_classes import Head, Tail
 import copy
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 
 
 def largest_grid_needed(lines):
@@ -22,27 +24,63 @@ def largest_grid_needed(lines):
         if direction == 'D':
             down += int(steps)
 
-    return np.zeros((2 * up + down, 2 * left + right))
+    return np.zeros((2 * (up + down), (left + right)))
 
-#what is going on with the lines textwrapper here? after looping through in function largest grid the other loop wont work anymore
+
+# what is going on with the lines textwrapper here? after looping through in function largest grid the other loop wont work anymore
 def day9_part1():
-    line1 = open('../data/day9_input.txt', 'r')
-    lines = open('../data/day9_input.txt', 'r')
+    line1 = open('../data/day9_test2.txt', 'r')
+    lines = open('../data/day9_test2.txt', 'r')
     Matrix = largest_grid_needed(lines)
 
     shape = Matrix.shape
     starting_position = (round(shape[0] / 2), round(shape[1] / 2))
     Matrix[starting_position] = 1
 
-    head = Head(starting_position[1], starting_position[0], Tail(starting_position[1], starting_position[0], Matrix))
+    head = Head(starting_position[1], starting_position[0],
+                Tail(1, starting_position[1], starting_position[0], Matrix, None))
 
     for line in line1:
-
         direction, steps = line.split()
         head.move(direction=direction, steps=int(steps))
 
-    #print(head.tail.matrix)
+    print(head.tail.matrix)
     return head.tail.matrix.sum()
 
 
-print(day9_part1())
+def day9_part2():
+    line1 = open('../data/day9_test.txt', 'r')
+    lines = open('../data/day9_test.txt', 'r')
+    Matrix = largest_grid_needed(lines)
+
+    shape = Matrix.shape
+    starting_position = (round(shape[0] / 2), round(shape[1] / 2))
+    Matrix[starting_position] = 1
+
+    head = Head(starting_position[1], starting_position[0],
+                Tail(1, starting_position[1], starting_position[0], None,
+                     Tail(2, starting_position[1], starting_position[0], None,
+                          Tail(3, starting_position[1], starting_position[0], None,
+                               Tail(4, starting_position[1], starting_position[0], None,
+                                    Tail(5, starting_position[1], starting_position[0], None,
+                                         Tail(6, starting_position[1], starting_position[0], None,
+                                              Tail(7, starting_position[1], starting_position[0], None,
+                                                   Tail(8, starting_position[1], starting_position[0], None,
+                                                        Tail(9, starting_position[1], starting_position[0], Matrix,
+                                                             None))))))))))
+
+    for line in line1:
+        direction, steps = line.split()
+        head.move(direction=direction, steps=int(steps))
+
+    # print(head.tail.matrix.sum())
+    # print(head.find_tail().matrix)
+    print(head.find_tail().id)
+    print(head.find_tail().matrix)
+    return head.find_tail().matrix.sum()
+
+
+print(day9_part2())
+
+
+
--- functions/day9_classes.py
+++ functions/day9_classes.py
@@ -1,6 +1,6 @@
 class Head:
     def __init__(self, x_pos, y_pos, tail):
-        self.x_pos= x_pos
+        self.x_pos = x_pos
         self.y_pos = y_pos
         self.tail = tail
 
@@ -14,9 +14,9 @@ class Head:
                 self._move(-1, 'x')
 
             if direction == "U":
-                self._move(1, 'y')
-            if direction == 'D':
                 self._move(-1, 'y')
+            if direction == 'D':
+                self._move(1, 'y')
 
     def _move(self, step, axis):
 
@@ -25,7 +25,7 @@ class Head:
             new_y = self.y_pos + step
 
             if abs(new_y - self.tail.y_pos) > 1 or abs(self.x_pos - self.tail.x_pos) > 1:
-                self.tail.move(self.x_pos, old_y)
+                self.tail.move(self.x_pos, old_y, axis, step)
 
             self.y_pos = new_y
 
@@ -34,24 +34,77 @@ class Head:
             new_x = self.x_pos + step
 
             if abs(self.y_pos - self.tail.y_pos) > 1 or abs(new_x - self.tail.x_pos) > 1:
-                self.tail.move(old_x, self.y_pos)
+                self.tail.move(old_x, self.y_pos, axis, step)
 
             self.x_pos = new_x
 
+    def find_tail(self):
+        if self.tail is not None:
+            return self._find_tail(self.tail)
+
+        return self
+
+    def _find_tail(self, Tail):
+
+        if Tail.tail is not None:
+            Tail = self._find_tail(Tail.tail)
+
+        return Tail
+
+
 class Tail:
-    def __init__(self, x_pos, y_pos, matrix, tail):
-        self.x_pos= x_pos
+    def __init__(self, id, x_pos, y_pos, matrix, tail):
+        self.id = id
+        self.x_pos = x_pos
         self.y_pos = y_pos
         self.matrix = matrix
 
         self.tail = tail
-    def move(self, x_pos, y_pos):
+
+    def move(self, x_pos, y_pos, axis, step):
+
+        if self.tail is not None:
+
+            self._move(x_pos, y_pos, step, axis)
+
+        else:
+            print(self.id)
+            print(self.x_pos, self.y_pos)
+            print(x_pos, y_pos)
+            self.x_pos = x_pos
+            self.y_pos = y_pos
+            self.add_trace()
+
+    def add_trace(self):
+        if self.matrix[(self.y_pos, self.x_pos)] == 0:
+            self.matrix[(self.y_pos, self.x_pos)] = 1
+
+    def _move(self, x_pos, y_pos, step, axis):
+
         self.x_pos = x_pos
         self.y_pos = y_pos
 
-        self.add_trace()
+        if abs(self.y_pos - self.tail.y_pos) > 1 or abs(self.x_pos - self.tail.x_pos) > 1:
 
-    def add_trace(self):
+            if self.y_pos != self.tail.y_pos and self.x_pos != self.tail.x_pos:
+                #print(self.id, self.tail.id)
+                #print(self.x_pos, self.tail.x_pos, self.y_pos, self.tail.y_pos)
 
-        if self.matrix[(self.y_pos, self.x_pos)] == 0:
-            self.matrix[(self.y_pos, self.x_pos)] = 1
+                if self.x_pos - self.tail.x_pos < 0 and self.y_pos - self.tail.y_pos < 0:
+                    self.tail.move(self.tail.x_pos - 1, self.tail.y_pos - 1, axis, step)
+
+                if self.x_pos - self.tail.x_pos > 0 and self.y_pos - self.tail.y_pos < 0:
+                    self.tail.move(self.tail.x_pos + 1, self.tail.y_pos - 1, axis, step)
+
+                if self.x_pos - self.tail.x_pos < 0 and self.y_pos - self.tail.y_pos > 0:
+                    self.tail.move(self.tail.x_pos - 1, self.tail.y_pos + 1, axis, step)
+
+                if self.x_pos - self.tail.x_pos > 0 and self.y_pos - self.tail.y_pos > 0:
+                    self.tail.move(self.tail.x_pos + 1, self.tail.y_pos + 1, axis, step)
+
+            else:
+                if axis == 'y':
+                    self.tail.move(self.tail.x_pos, self.tail.y_pos + step, axis, step)
+
+                if axis == 'x':
+                    self.tail.move(self.tail.x_pos + step, self.tail.y_pos, axis, step)
